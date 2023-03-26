1c52df3fb1b1a226cc122b3133921456d1df6ea0
Xavier Lange <xrlange@gmail.com>
2023-03-25 21:22:35 -0400

    drag in aws sdk for a comparison

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use rusoto_sts::{GetCallerIdentityRequest, Sts, StsClient};
 
+
 #[tokio::main]
 async fn main() {
+    rusoto().await;
+
+    aws_sdk().await;
+}
+
+async fn aws_sdk() {
+    let shared_config = aws_config::load_from_env().await;
+    let client = aws_sdk_sts::Client::new(&shared_config);
+    let req = client.get_caller_identity();
+    let result = req.send().await;
+    println!("identity: {:?}", result)
+}
+
+async fn rusoto() {
     let client = StsClient::new(rusoto_core::Region::default());
     let identity = client.get_caller_identity(GetCallerIdentityRequest{}).await;
     println!("default chain says: {:?}", identity);
@@ -9,7 +24,7 @@ async fn main() {
     let provider = rusoto_sts::WebIdentityProvider::from_k8s_env();
     let provider = rusoto_credential::AutoRefreshingProvider::new(provider).unwrap();
     let client = StsClient::new_with(rusoto_core::HttpClient::new().unwrap(),
-    provider, rusoto_core::Region::default());
+                                     provider, rusoto_core::Region::default());
     let identity = client.get_caller_identity(GetCallerIdentityRequest{}).await;
     println!("default chain says: {:?}", identity)
 }
