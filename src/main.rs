e8ff5abb05d6263b7b640a1ae98d5dd6515c3143
Xavier Lange <xrlange@gmail.com>
2023-03-25 21:05:51 -0400

    try custom provider

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,12 @@ use rusoto_sts::{GetCallerIdentityRequest, Sts, StsClient};
 async fn main() {
     let client = StsClient::new(rusoto_core::Region::default());
     let identity = client.get_caller_identity(GetCallerIdentityRequest{}).await;
-    println!("{:?}", identity);
+    println!("default chain says: {:?}", identity);
+
+    let provider = rusoto_sts::WebIdentityProvider::from_k8s_env();
+    let provider = rusoto_credential::AutoRefreshingProvider::new(provider)?;
+    let client = StsClient::new_with(rusoto_core::HttpClient::new().unwrap(),
+    provider, rusoto_core::Region::default());
+    let identity = client.get_caller_identity(GetCallerIdentityRequest{}).await;
+    println!("default chain says: {:?}", identity)
 }
